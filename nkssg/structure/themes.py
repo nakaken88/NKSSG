dbf79d60ac754430dc5be647bde8234bfec1d7c0
nakaken88 <nakaken88888888@gmail.com>
2021-04-05 21:00:19 +0900

    feat: enabled to get data from theme config

--- nkssg/structure/themes.py
+++ nkssg/structure/themes.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 import pkg_resources
 
+from ruamel.yaml import YAML
+
 from nkssg.utils import get_config_by_list
 
 
 class Themes:
     def __init__(self, config):
         self.dirs = []
+        self.cnf = {}
 
         themes_dir = config['base_dir'] / 'themes'
 
@@ -15,6 +18,10 @@ class Themes:
             theme_dir = themes_dir / theme
             if theme_dir.exists():
                 self.dirs.append(theme_dir)
+                cnf_path = theme_dir / (theme + '.yml')
+                if cnf_path.exists():
+                    cnf = YAML(typ='safe').load(cnf_path) or {}
+                    self.cnf = cnf
             else:
                 print(theme + ' is not found')
 
@@ -23,6 +30,10 @@ class Themes:
             theme_dir = themes_dir / theme
             if theme_dir.exists():
                 self.dirs.append(theme_dir)
+                cnf_path = theme_dir / (theme + '.yml')
+                if cnf_path.exists():
+                    cnf = YAML(typ='safe').load(cnf_path) or {}
+                    self.cnf = {**cnf, **self.cnf}
             else:
                 print(theme + ' is not found')
 
@@ -30,3 +41,7 @@ class Themes:
             theme_dir = config["PKG_DIR"] / 'themes' / 'default'
             self.dirs.append(theme_dir)
 
+            cnf_path = theme_dir / 'default.yml'
+            if cnf_path.exists():
+                cnf = YAML(typ='safe').load(cnf_path) or {}
+                self.cnf = cnf
