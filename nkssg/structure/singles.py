e2e39bf8b7c3dd1b201d946eb78d07cefe51a7da
nakaken88 <nakaken88888888@gmail.com>
2022-01-30 09:56:16 +0900

    refactor: add "before/after_render_html" flag

--- nkssg/structure/singles.py
+++ nkssg/structure/singles.py
@@ -551,9 +551,13 @@ class Single(Page):
 
             self.content = content
 
+        self.content = config['plugins'].do_action('before_render_html', target=self.content, config=config, single=self)
+
         self.html = template.render({
             'mypage': self,
             })
+
+        self.html = config['plugins'].do_action('after_render_html', target=self.html, config=config, single=self)
 
 
 
