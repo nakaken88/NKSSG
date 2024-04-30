65ef66b4a239b3bad9019bff928c1bc960371bb7
nakaken88 <nakaken88888888@gmail.com>
2024-04-30 19:19:16 +0900

    update: drop Python 3.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='nakaken88',
     packages=find_packages(),
     include_package_data=True,
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     install_requires=[
         'beautifulsoup4',
         'click',
