739a3b2d6d030855e79e02471643ac2421754525
nakaken88 <nakaken88888888@gmail.com>
2024-05-05 13:06:18 +0900

    refactor: build command processing

--- nkssg/command/build.py
+++ nkssg/command/build.py
@@ -11,7 +11,6 @@ from nkssg.structure.site import Site
 def build(config: Config, clean=False):
 
     site = Site(config)
-
     site.setup()
     site.update()
 
@@ -20,17 +19,11 @@ def build(config: Config, clean=False):
         if public_dir.exists():
             shutil.rmtree(public_dir)
 
-        public_dir.mkdir()
-
+    public_dir.mkdir(exist_ok=True)
     site.output()
 
 
-def serve(config: Config, static=False, port=5500):
-
-    config.site.site_url = 'http://127.0.0.1:' + str(port)
-    config.public_dir = Path(tempfile.mkdtemp(prefix='nkssg_'))
-    print(str(config.public_dir) + ' is created.')
-
+def start_server(config: Config, watch_path_list, port=5500):
     def reload():
         build(config)
 
@@ -38,14 +31,26 @@ def serve(config: Config, static=False, port=5500):
 
     try:
         server = Server()
-        if not static:
-            server.watch(config.docs_dir, reload)
-            if config.themes_dir.exists():
-                server.watch(config.themes_dir, reload)
-
+        for watch_path in watch_path_list:
+            server.watch(str(watch_path), reload)
         server.serve(port=port, root=config.public_dir, open_url_delay=None)
     finally:
         shutil.rmtree(config.public_dir)
+
+
+def serve(config: Config, static=False, port=5500):
+
+    config.site.site_url = f'http://127.0.0.1:{port}'
+    config.public_dir = Path(tempfile.mkdtemp(prefix='nkssg_'))
+    print(f'{config.public_dir} is created.')
+
+    watch_path_list = []
+    if not static:
+        watch_path_list.append(config.docs_dir)
+        if config.themes_dir.exists():
+            watch_path_list.append(config.themes_dir)
+
+    start_server(config, watch_path_list, port)
 
 
 def draft(config: Config, path, port=5500):
@@ -55,17 +60,7 @@ def draft(config: Config, path, port=5500):
         draft_path = config.base_dir / draft_path
 
     config['draft_path'] = draft_path
-    config.site.site_url = 'http://127.0.0.1:' + str(port)
+    config.site.site_url = f'http://127.0.0.1:{port}'
     config.public_dir = Path(tempfile.mkdtemp(prefix='nkssg_'))
 
-    def reload():
-        build(config)
-
-    reload()
-
-    try:
-        server = Server()
-        server.watch(config['draft_path'].parent, reload)
-        server.serve(port=port, root=config.public_dir, open_url_delay=None)
-    finally:
-        shutil.rmtree(config.public_dir)
+    start_server(config, [config['draft_path']], port)
