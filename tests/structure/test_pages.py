fce8c12afb653b76fb4488e43de6d36e35f2d7ee
nakaken88 <nakaken88888888@gmail.com>
2024-05-17 19:44:28 +0900

    refactor: move test for to_slug to pages

--- tests/structure/test_pages.py
+++ tests/structure/test_pages.py
@@ -44,3 +44,27 @@ def test_clean_name_underscore_double():
 def test_clean_name_underscore_triple():
     ret = Page.clean_name('___sample')
     assert ret == '__sample'
+
+
+def test_to_slug_no_change():
+    dirty_slug = 'sample'
+    ret = Page.to_slug(dirty_slug)
+    assert ret == 'sample'
+
+
+def test_to_slug_space():
+    dirty_slug = 'a b c'
+    ret = Page.to_slug(dirty_slug)
+    assert ret == 'a-b-c'
+
+
+def test_to_slug_upper():
+    dirty_slug = 'Sample'
+    ret = Page.to_slug(dirty_slug)
+    assert ret == 'sample'
+
+
+def test_to_slug_space_upper():
+    dirty_slug = 'A of C'
+    ret = Page.to_slug(dirty_slug)
+    assert ret == 'a-of-c'
