e1c538ecb243831afbe8d58a30873d0cbd520abe
nakaken88 <nakaken88888888@gmail.com>
2024-06-02 07:46:42 +0900

    refactor: test cases for `clean_name` and `to_slug` functions using pytest parameterization

--- tests/structure/test_pages.py
+++ tests/structure/test_pages.py
@@ -1,70 +1,33 @@
+import pytest
+
 from nkssg.structure.pages import Page
 
 
-def test_clean_name_no_underscore():
-    ret = Page.clean_name('sample')
-    assert ret == 'sample'
+@pytest.mark.parametrize("input, expected", [
+    ('sample', 'sample'),       # unchange (no _)
+    ('_10_sample', 'sample'),   # drop prefix
+    ('_sample', '_sample'),     # unchange (one _)
+    ('_sample_', '_sample_'),   # unchange (end with _, no suffix)
+    ('_a_b_c', 'b_c'),          # case three _
+    ('_a_b_', 'b_'),            # case three _ (end with _)
+    ('a_b_c', 'a_b_c'),         # unchange (not start with _)
+    ('_!@#_a b c', 'a b c'),    # special words
+    ('__sample', '_sample'),    # start __
+    ('___sample', '__sample'),  # start ___
+    ('_____', '____'),          # only _
+    ('', ''),                   # no words
+])
+def test_clean_name(input, expected):
+    assert Page.clean_name(input) == expected
 
 
-def test_clean_name_underscore_normal():
-    ret = Page.clean_name('_10_sample')
-    assert ret == 'sample'
-
-
-def test_clean_name_underscore_first():
-    ret = Page.clean_name('_sample')
-    assert ret == '_sample'
-
-
-def test_clean_name_underscore_first_last():
-    ret = Page.clean_name('_sample_')
-    assert ret == '_sample_'
-
-
-def test_clean_name_underscore_first_middle_last():
-    ret = Page.clean_name('_sample_sample_')
-    assert ret == 'sample_'
-
-
-def test_clean_name_underscore_middle():
-    ret = Page.clean_name('sample_sample')
-    assert ret == 'sample_sample'
-
-
-def test_clean_name_underscore_first_middle_middle():
-    ret = Page.clean_name('_sample1_sample2_sample3')
-    assert ret == 'sample2_sample3'
-
-
-def test_clean_name_underscore_double():
-    ret = Page.clean_name('__sample')
-    assert ret == '_sample'
-
-
-def test_clean_name_underscore_triple():
-    ret = Page.clean_name('___sample')
-    assert ret == '__sample'
-
-
-def test_to_slug_no_change():
-    dirty_slug = 'sample'
-    ret = Page.to_slug(dirty_slug)
-    assert ret == 'sample'
-
-
-def test_to_slug_space():
-    dirty_slug = 'a b c'
-    ret = Page.to_slug(dirty_slug)
-    assert ret == 'a-b-c'
-
-
-def test_to_slug_upper():
-    dirty_slug = 'Sample'
-    ret = Page.to_slug(dirty_slug)
-    assert ret == 'sample'
-
-
-def test_to_slug_space_upper():
-    dirty_slug = 'A of C'
-    ret = Page.to_slug(dirty_slug)
-    assert ret == 'a-of-c'
+@pytest.mark.parametrize("dirty_slug, expected", [
+    ('sample', 'sample'),       # unchange
+    ('a b c', 'a-b-c'),         # space
+    ('Sample', 'sample'),       # upper case
+    ('A of C', 'a-of-c'),       # space + upper case
+    ('a  b   c', 'a--b---c'),   # multi space
+    ('a!b@c', 'a!b@c'),         # special words
+])
+def test_to_slug(dirty_slug, expected):
+    assert Page.to_slug(dirty_slug) == expected
