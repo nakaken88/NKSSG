4552c00003925af301aa8902c7eb225fad3f8f04
nakaken88 <nakaken88888888@gmail.com>
2024-06-23 13:42:20 +0900

    refactor: implement backlink extraction and cleaning functionality
    - Introduce regex pattern to extract href attribute values from <a> tags.
    - Add static method `extract_urls_from_html` to extract URLs from HTML content.
    - Add static method `clean_url` to clean extracted URLs by:
      - Removing site URL or original site URL.
      - Removing fragments and query strings.
      - Converting URLs to lowercase for consistency.

--- nkssg/plugins/backlink.py
+++ nkssg/plugins/backlink.py
@@ -1,36 +1,56 @@
-from nkssg.structure.plugins import BasePlugin
 import re
 from urllib.parse import quote
 
+from nkssg.structure.config import Config
+from nkssg.structure.plugins import BasePlugin
+from nkssg.structure.site import Site
+
 
 class BacklinkPlugin(BasePlugin):
-    def after_update_urls(self, site, **kwargs):
-        target = site.singles
-        pattern = re.compile(r'<a[^<>]+href\s*?=\s*?["\'](.*?)["\']', re.I)
-        for page in target:
-            to_links_text = set()
-            for tag in pattern.finditer(page.content):
-                href = tag.group(1)
-                if 'http' in href:
-                    if site.config['site']['site_url_original'] in href:
-                        href = href.replace(site.config['site']['site_url_original'], '')
-                    else:
-                        continue
-                if '#' in href:
-                    href = href[:(href.find('#'))]
-                if '?' in href:
-                    href = href[:(href.find('?'))]
-                if href.replace('/', '') == '':
-                    continue
-                to_links_text.add(href)
-            page.to_links_text = to_links_text
+
+    href_pattern = re.compile(
+        r'<a\s+'                # Opening tag and space
+        r'[^>]*?'               # Any attributes
+        r'href\s*=\s*'          # href attribute
+        r'["\'](.*?)["\']',     # Attribute value
+        re.I | re.S
+    )
+
+    @staticmethod
+    def extract_urls_from_html(html_content):
+        return BacklinkPlugin.href_pattern.findall(html_content)
+
+    @staticmethod
+    def clean_url(href: str, config: Config):
+        href = href.lower()
+
+        if 'http' in href:
+            if config.site.site_url in href:
+                href = href.replace(config.site.site_url, '')
+            elif config.site.site_url_original in href:
+                href = href.replace(config.site.site_url_original, '')
+            else:
+                return None
+
+        href = href.split('#')[0]
+        href = href.split('?')[0]
+        return href
+
+    def after_update_urls(self, site: Site, **kwargs):
+        for page in site.singles:
             page.to_links = set()
             page.back_links = set()
+            page.to_links_text = set()
 
-        urls = {str(page.rel_url): page for page in target}
-        for page in target:
+            for href in BacklinkPlugin.extract_urls_from_html(page.content):
+                cleaned_href = BacklinkPlugin.clean_url(href, site.config)
+                if cleaned_href:
+                    page.to_links_text.add(cleaned_href)
+
+        urls = {str(page.rel_url).lower(): page for page in site.singles}
+        for page in site.singles:
             for link in page.to_links_text:
-                quote_link = quote(link).lower()
-                if quote_link in urls:
-                    page.to_links.add(urls[quote_link])
-                    urls[quote_link].back_links.add(page)
+                to_page = urls.get(quote(link))
+                if to_page:
+                    page.to_links.add(to_page)
+                    to_page.back_links.add(page)
