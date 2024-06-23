ed02eb38010133763e64a3bbf3f442761dd0c1cd
nakaken88 <nakaken88888888@gmail.com>
2024-06-23 09:14:53 +0900

    refactor: add type hint to select_pages plugin

--- nkssg/plugins/select_pages.py
+++ nkssg/plugins/select_pages.py
@@ -1,14 +1,15 @@
 from nkssg.structure.plugins import BasePlugin
+from nkssg.structure.singles import Singles
 
 
 class SelectPagesPlugin(BasePlugin):
-    def after_initialize_singles(self, singles):
-        mode = singles.config.get('mode') or ''
+    def after_initialize_singles(self, singles: Singles):
+        mode = singles.config.get('mode', '')
         if mode != 'serve':
             return singles
 
-        start = self.config.get('start') or 0
-        end = self.config.get('end') or len(singles.pages)
-        step = self.config.get('step') or 1
+        start = self.config.get('start', 0)
+        end = self.config.get('end', len(singles.pages))
+        step = self.config.get('step', 1)
         singles.pages = singles.pages[start:end:step]
         return singles
